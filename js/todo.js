a861edb5cefe3a442b70a218d8c3851eafd0e609
RYAN-PC\Ryan <john1049@algonquinlive.com>
2017-04-23 16:13:19 -0400

    CRUD functions implemented

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,69 @@
-(function() {
-    var app = angular.module('todoApp', ['ngAnimate', 'angular.filter'])
-    app.controller('todoController', function($scope) {
+angular
+    .module('todoApp', ['firebase', 'angular.filter'])
+    .constant('firebaseConfig', {
+      apiKey: "AIzaSyCGz88kNQ2b-7nU8EGaQHtubdnlisG54hI",
+      authDomain: "cloud-based-todo.firebaseapp.com",
+      databaseURL: "https://cloud-based-todo.firebaseio.com",
+      projectId: "cloud-based-todo",
+      storageBucket: "cloud-based-todo.appspot.com",
+      messagingSenderId: "509749300206"
+  })
+
+    .run(firebaseConfig => firebase.initializeApp(firebaseConfig))
+    .service('dbRefRoot', DbRefRoot)
+    .service('todos', Todos)
+    .controller('TodoCtrl', TodoCtrl)
+
+    function DbRefRoot() {
+      return firebase.database().ref()
+    }
+
+    function Todos(dbRefRoot, $firebaseObject, $firebaseArray) {
+      const dbRefTodos = dbRefRoot.child('todos')
+
+      this.get = function get(id) {
+        return $firebaseObject(dbRefTodos.child(id))
+      }
+
+      this.getAll = function getAll() {
+        return $firebaseArray(dbRefTodos)
+      }
+    }
+
+    function TodoCtrl(todos){
+
+      this.getNewTodo = function getNewTodo(){
+        return{
+          task_name: '',
+          due_date: '',
+          comment: '',
+          priority: ''
+        }
+      }
+
+      this.newTodo = this.getNewTodo();
+
+      this.todos = todos.getAll();
+
+      this.addTodo = function addTodo(newTodo){
+        this.todos
+          .$add(newTodo)
+          .then(newRef => {
+            this.newTodo = this.getNewTodo()
+          })
+      }
+
+      this.remove = function remove(todo){
+        this.todos.$remove(todo)
+      }
+
+      this.save = function save(contact) {
+        this.todos.$save(todo)
+      }
+    }
+
+/*
+    app.controller('TodoCtrl', function($scope) {
         $scope.tasks = (localStorage.getItem('todo') != null) ? JSON.parse(localStorage.getItem('todo')) : [];
         $scope.visible = false;
 
@@ -45,5 +108,4 @@
             this.task.complete = false;
             $scope.update();
         }
-    })
-})();
+    })*/
